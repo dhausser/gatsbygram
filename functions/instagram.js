8b1eeb1c2137509f7f02904038a615ac71debffd
Davy Hausser <davy.hausser@gmail.com>
2020-04-04 22:10:36 +0200

    instagram id update

--- functions/instagram.js
+++ functions/instagram.js
@@ -1,6 +1,6 @@
 require("isomorphic-fetch")
 
-const url = `https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables={"id":"21051932","first":8}`
+const url = `https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables={"id":"519208","first":8}`
 
 const cache = {
   lastFetch: 0,
