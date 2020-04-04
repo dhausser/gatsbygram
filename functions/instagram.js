67999ffa574fca2f0174cf2ff76f1eb9f831d142
Davy Hausser <davy.hausser@gmail.com>
2020-04-04 23:44:41 +0200

    instagram id update

--- functions/instagram.js
+++ functions/instagram.js
@@ -2,20 +2,20 @@ require("isomorphic-fetch")
 
 const url = `https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables={"id":"21051932","first":8}`
 
-const cache = {
-  lastFetch: 0,
-  posts: [],
-}
+// const cache = {
+//   lastFetch: 0,
+//   posts: [],
+// }
 
 async function getPosts() {
   // first see if we have a cache in 30 mins
-  const timeSinceLastFetch = Date.now() - cache.lastFetch
-  if (timeSinceLastFetch <= 1800000) {
-    return cache.posts
-  }
+  // const timeSinceLastFetch = Date.now() - cache.lastFetch
+  // if (timeSinceLastFetch <= 1800000) {
+  //   return cache.posts
+  // }
   const data = await fetch(url).then(res => res.json())
-  // const posts = slimUpPosts(data)
-  const posts = data
+  const posts = slimUpPosts(data)
+  // const posts = data
   cache.lastFetch = Date.now()
   cache.posts = posts
   return posts
