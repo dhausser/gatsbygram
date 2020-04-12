0f553a39616ed3c54da0cae4dee6e572fcb7d87b
Davy Hausser <davy.hausser@gmail.com>
2020-04-12 19:28:28 +0200

    instagram tile fix

--- src/components/instagram.js
+++ src/components/instagram.js
@@ -1,13 +1,6 @@
 import React, { useState, useEffect } from "react"
-import styled from "@emotion/styled"
-import { FaInstagram } from "react-icons/fa"
 import Card from "./card"
-
-const InstaStyles = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 1rem;
-`
+import { Box } from "@chakra-ui/core"
 
 function useInstagram() {
   const [posts, setPosts] = useState([])
@@ -24,29 +17,12 @@ function useInstagram() {
 export default function Instagram() {
   const gramz = useInstagram()
   return (
-    <div>
-      <h3>
-        <span className="highlight">
-          <a
-            href="https://instagram.com/davy_hausser"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaInstagram style={{ strokeWidth: 15 }} />
-            @davy_hausser{` `}
-          </a>
-          Instant Grams
-        </span>
-      </h3>
-      {!gramz.length && <p>One sec, getting the gramz...</p>}
-      {gramz.length ? <h4>Posts</h4> : null}
-      <InstaStyles>
-        {gramz.map(gram => (
-          <a href={gram.url} key={gram.id}>
-            <Card imageUrl={gram.thumbnail} imageAlt={gram.caption} />
-          </a>
-        ))}
-      </InstaStyles>
-    </div>
+    <Box>
+      {gramz.map(gram => (
+        <a href={gram.url} key={gram.id}>
+          <Card imageUrl={gram.thumbnail} imageAlt={gram.caption} />
+        </a>
+      ))}
+    </Box>
   )
 }
